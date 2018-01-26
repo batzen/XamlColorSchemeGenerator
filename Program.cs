01a81b7c413255ef2cb2bf695378bf8d105bbfa4
Bastian Schmidt <batzen@gmx.org>
2018-01-26 17:19:00 +0100

    Adding mutex to prevent concurrency issues

--- Program.cs
+++ Program.cs
@@ -3,6 +3,11 @@
     using System;
     using System.Diagnostics;
     using System.Linq;
+    using System.Reflection;
+    using System.Runtime.InteropServices;
+    using System.Security.Cryptography;
+    using System.Text;
+    using System.Threading;
 
     internal class Program
     {
@@ -15,8 +20,14 @@
                 // TODO: Add flags for some parameters.
                 if (args.Any())
                 {
-                    var generator = new Generator();
-                    generator.Generate(args[0]);
+                    var inputFile = args[0];
+
+                    using (Lock(inputFile))
+                    {
+                        var generator = new Generator();
+
+                        generator.Generate(inputFile);
+                    }
                 }
                 else
                 {
@@ -44,5 +55,34 @@
                 return 1;
             }
         }
+
+        private static IDisposable Lock(string file)
+        {
+            var appGuid = ((GuidAttribute)Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(GuidAttribute), false).GetValue(0)).Value;
+            var mutexName = $"Local\\{appGuid}_{GetMD5Hash(file)}";
+
+            var mutex = new Mutex(false, mutexName);
+
+            if (mutex.WaitOne(TimeSpan.FromSeconds(10)) == false)
+            {
+                throw new TimeoutException("Another instance of this application blocked the concurrent execution.");
+            }
+
+            return mutex;
+        }
+
+        private static string GetMD5Hash(string textToHash)
+        {
+            if (string.IsNullOrEmpty(textToHash))
+            {
+                return string.Empty;
+            }
+
+            var md5 = new MD5CryptoServiceProvider();
+            var bytesToHash = Encoding.Default.GetBytes(textToHash);
+            var result = md5.ComputeHash(bytesToHash); 
+
+            return BitConverter.ToString(result); 
+        } 
     }
 }
