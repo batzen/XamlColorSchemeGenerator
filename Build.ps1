b118b79d594345c43adae395679f64a649017d27
Bastian Schmidt <batzen@gmx.org>
2018-01-27 09:23:34 +0100

    Adding build scripts

---
+++ Build.ps1
@@ -0,0 +1,30 @@
+Param(
+    [Parameter(Mandatory=$False)]
+    [string]$Target = "Build",    
+    [Parameter(Mandatory=$False)]
+    [string]$Configuration = "Release",
+    [Parameter(Mandatory=$False)]
+    [string]$PreRelease
+)
+
+$ErrorActionPreference = "Stop"
+
+. $PSScriptRoot\MSBuildHelper.ps1
+
+if ($PSBoundParameters['Verbose']) {
+    $verbosity = "d"
+} else {
+    $verbosity = "m"
+}
+
+Write-Output "Building with verbosity '$verbosity'"
+
+$msbuild = Get-MSBuild
+
+$measure = Measure-Command {
+	&$msbuild XamlColorSchemeGenerator.msbuild /target:$Target /property:Configuration=$Configuration /property:Prerelease=$PreRelease /v:$verbosity /nologo | Out-Default
+}
+
+Write-Output "Time elapsed $($measure.ToString())"
+
+if ($LASTEXITCODE -ne 0) { exit 1 }
