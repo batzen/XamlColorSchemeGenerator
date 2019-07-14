7d3f68a09164834103c03c7b18327a96e65ac327
Bastian Schmidt <batzen@gmx.org>
2019-07-14 23:50:02 +0200

    Stopping stopwatch after mutex release

--- src/Program.cs
+++ src/Program.cs
@@ -38,6 +38,7 @@
                         finally
                         {
                             mutex.ReleaseMutex();
+                            stopwatch.Stop();
                         }
                     }
                 }
